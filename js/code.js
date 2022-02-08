3ca2f93f955ba9fd023c3f5d9549219cb7699c35
muratbyk05 <95565340+muratbyk05@users.noreply.github.com>
2022-02-08 21:25:36 +0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,6 @@
+function das(){
+    document.body.style.backgroundColor="red";
+    document.body.style.Color="yellow";
+    document.body.style.fontSize="5rem";
+    document.getElementById("logo").setAttribute("src","images/logo.png")
+}                   
